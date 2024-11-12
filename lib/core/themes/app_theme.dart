87ad4436969dd8a0d2207471213f42ebaa5aa5c3
alvarado0925 <alvarado0925@gmail.com>
2024-11-12 00:43:05 -0500

    Añade soporte para temas personalizados: implementa la clase AppTheme y aplica el tema seleccionado en la aplicación.

---
+++ lib/core/themes/app_theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const Color _customColor = Color(0xFF5C11D4);
+
+const List<Color> _colorThemes = [
+  _customColor,
+  Colors.blue,
+  Colors.red,
+  Colors.green,
+  Colors.orange,
+  Colors.purple,
+  Colors.pink,
+  Colors.teal,
+  Colors.amber,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor >= 0 && selectedColor < _colorThemes.length,
+            "Colors must be between 0 and ${_colorThemes.length - 1}");
+
+  ThemeData theme() {
+    return ThemeData(
+      colorSchemeSeed: _colorThemes[selectedColor],
+    );
+  }
+}
