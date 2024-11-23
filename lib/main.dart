fc694211ac25ca54480f22072f428303509e557e
alvarado0925 <alvarado0925@gmail.com>
2024-11-22 23:06:44 -0500

    Añade soporte para Provider en la aplicación y configura ChatProvider en MyApp.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:yes_no_app/themes/app_theme.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/providers/chat_provider.dart';
 import 'package:yes_no_app/screens/chat_screen.dart';
+import 'package:yes_no_app/themes/app_theme.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,10 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Material App',
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme(selectedColor: 5).theme(),
-        home: const ChatScreen());
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => ChatProvider())],
+      child: MaterialApp(
+          title: 'Material App',
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme(selectedColor: 5).theme(),
+          home: const ChatScreen()),
+    );
   }
 }
